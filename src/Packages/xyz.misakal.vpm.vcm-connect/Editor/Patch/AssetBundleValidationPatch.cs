801ef345ff6a3489a2bba881ddd2ff5ddb6b6aef
Misaka-L <lipww1234@foxmail.com>
2025-10-18 13:51:01 +0800

    feat: remove bundle size check

---
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/Patch/AssetBundleValidationPatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using HarmonyLib;
+using UnityEngine;
+using VRC;
+using VRC.SDKBase.Editor.Validation;
+
+namespace VRChatContentManagerConnect.Editor.Patch {
+    // public static bool CheckIfAssetBundleFileTooLarge(
+    //  ContentType contentType,
+    //  string vrcFilePath,
+    //  out int fileSize,
+    //  bool mobilePlatform
+    // )
+    [HarmonyPatch(typeof(ValidationEditorHelpers), nameof(ValidationEditorHelpers.CheckIfAssetBundleFileTooLarge))]
+    internal static class AssetBundleValidationPatch {
+        public static bool Prefix(ref bool __result, ContentType contentType, string vrcFilePath, ref int fileSize,
+            bool mobilePlatform) {
+            fileSize = 0;
+            __result = true;
+
+            if (!File.Exists(vrcFilePath)) {
+                Debug.LogError("[VRCCM.Connect] Failed to validate asset bundle size: file does not exist at path " +
+                               vrcFilePath);
+                return true;
+            }
+
+            try {
+                fileSize = (int)new FileInfo(vrcFilePath).Length;
+                __result = false;
+                return false;
+            }
+            catch (Exception ex) {
+                Debug.LogException(ex);
+                Debug.LogError("[VRCCM.Connect] Failed to validate asset bundle size: exception occurred when accessing file at path " + vrcFilePath);
+                return false;
+            }
+        }
+    }
+}
