f01bea66cfe7ab0a10edf9704ea85daa0e68f58f
Misaka-L <lipww1234@foxmail.com>
2025-10-24 14:02:48 +0800

    feat: delayed delete bundle file

--- src/Packages/xyz.misakal.vpm.vcm-connect.avatars/Editor/Patch/AvatarBuilderPatch.cs
+++ src/Packages/xyz.misakal.vpm.vcm-connect.avatars/Editor/Patch/AvatarBuilderPatch.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Threading;
 using HarmonyLib;
 using UnityEditor;
 using UnityEngine;
 using VRC.SDK3.Builder;
+using VRChatContentManagerConnect.Editor;
 
 namespace VRChatContentManagerConnect.Avatars.Editor.Patch {
     //   private static bool ExportCurrentAvatarResource(
@@ -46,117 +46,17 @@ namespace VRChatContentManagerConnect.Avatars.Editor.Patch {
             buildOptionsCode.opcode = OpCodes.Ldc_I4_S;
             buildOptionsCode.operand = (sbyte)BuildAssetBundleOptions.UncompressedAssetBundle;
 
-            var getTemporaryCachePathMethodInfo = AccessTools.PropertyGetter(typeof(Application),
-                nameof(Application.temporaryCachePath));
-            var assetBundleNameFieldInfo =
-                AccessTools.Field(typeof(AssetBundleBuild), nameof(AssetBundleBuild.assetBundleName));
+            var deleteCallCodeMatcher = new CodeMatcher(codes)
+                .MatchStartForward(
+                    CodeMatch.Calls(() => File.Delete(default))
+                )
+                .ThrowIfInvalid("[VRCCM.Connect] AvatarBuilderPatch: Could not find File.Delete calls.")
+                .RemoveInstruction()
+                .InsertAndAdvance(
+                    CodeInstruction.Call(() => DelayedDelete.Delete(default))
+                );
 
-            var pathCombineMethodInfo = AccessTools.Method(typeof(System.IO.Path),
-                nameof(System.IO.Path.Combine),
-                new[] { typeof(string), typeof(string) });
-
-            var pathCombineCalls = codes
-                .Where(code => code.opcode == OpCodes.Call &&
-                               code.operand is MethodInfo methodInfo &&
-                               methodInfo == pathCombineMethodInfo)
-                .ToArray();
-            if (pathCombineCalls.Length == 0) {
-                Debug.LogError("[VRCCM.Connect] AvatarBuilderPatch: Could not find any PathCombine calls.");
-                return originalCodes;
-            }
-
-            LocalBuilder bundlePathLocal = null;
-            foreach (var pathCombineCall in pathCombineCalls) {
-                var callIndex = codes.IndexOf(pathCombineCall);
-                var firstArgIndex = callIndex - 3;
-                var secArgIndex = callIndex - 1;
-                var localIndex = callIndex + 1;
-                if (secArgIndex - 1 < 0 || firstArgIndex - 1 < 0 || localIndex >= codes.Count)
-                    continue;
-
-                var firstArg = codes[firstArgIndex];
-                var secArg = codes[secArgIndex];
-                var local = codes[localIndex];
-
-                if (
-                    // Application.temporaryCachePath
-                    firstArg.opcode == OpCodes.Call && firstArg.operand is MethodInfo methodInfo &&
-                    methodInfo == getTemporaryCachePathMethodInfo &&
-                    // assetBundleBuild.assetBundleName
-                    secArg.opcode == OpCodes.Ldfld && secArg.operand is FieldInfo fieldInfo &&
-                    fieldInfo == assetBundleNameFieldInfo) {
-                    if (local.opcode == OpCodes.Stloc_S && local.operand is LocalBuilder localBuilder) {
-                        bundlePathLocal = localBuilder;
-                        break;
-                    }
-                }
-            }
-
-            if (bundlePathLocal is null) {
-                Debug.LogError("[VRCCM.Connect] AvatarBuilderPatch: Could not find bundle path local variable.");
-                return originalCodes;
-            }
-            
-            var sessionStateGetStringMethodInfo = AccessTools.Method(typeof(SessionState), nameof(SessionState.GetString),
-                new[] { typeof(string), typeof(string) });
-
-            LocalBuilder previousBuildingAssetBundlePathLocal = null;
-            var sessionStateGetStringCalls = codes
-                .Where(code => code.opcode == OpCodes.Call &&
-                               code.operand is MethodInfo methodInfo &&
-                               methodInfo == sessionStateGetStringMethodInfo)
-                .ToArray();
-            foreach (var sessionStateGetStringCall in sessionStateGetStringCalls) {
-                var callIndex = codes.IndexOf(sessionStateGetStringCall);
-                var firstArgIndex = callIndex - 2;
-                
-                if (firstArgIndex < 0 || firstArgIndex >= codes.Count)
-                    continue;
-                
-                var firstArg = codes[firstArgIndex];
-                if (firstArg.opcode == OpCodes.Ldstr && firstArg.operand is "previousBuildingAssetBundlePath") {
-                    var localIndex = callIndex + 1;
-                    if (localIndex < 0 || localIndex >= codes.Count)
-                        continue;
-
-                    var local = codes[localIndex];
-                    if (local.opcode == OpCodes.Stloc_S && local.operand is LocalBuilder localBuilder) {
-                        previousBuildingAssetBundlePathLocal = localBuilder;
-                        break;
-                    }
-                }
-            }
-
-            if (previousBuildingAssetBundlePathLocal is null) {
-                Debug.LogError("[VRCCM.Connect] AvatarBuilderPatch: Could not find previousBuildingAssetBundlePath local variable.");
-                return originalCodes;
-            }
-
-            var fileDeleteMethodInfo = AccessTools.Method(typeof(System.IO.File),
-                nameof(System.IO.File.Delete),
-                new[] { typeof(string) });
-
-            var fileDeleteCalls = codes.Where(code =>
-                    code.opcode == OpCodes.Call && code.operand is MethodInfo methodInfo &&
-                    methodInfo == fileDeleteMethodInfo)
-                .ToArray();
-            foreach (var fileDeleteCall in fileDeleteCalls) {
-                var callIndex = codes.IndexOf(fileDeleteCall);
-                var argIndex = callIndex - 1;
-                if (argIndex < 0)
-                    continue;
-
-                var arg = codes[argIndex];
-                if (arg.opcode == OpCodes.Ldloc_S && arg.operand is LocalBuilder localBuilder &&
-                    (localBuilder == bundlePathLocal || localBuilder == previousBuildingAssetBundlePathLocal)) {
-                    codes[argIndex].opcode = OpCodes.Nop;
-                    codes[argIndex].operand = null;
-                    codes[callIndex].opcode = OpCodes.Nop;
-                    codes[callIndex].operand = null;
-                }
-            }
-
-            return codes;
+            return deleteCallCodeMatcher.Instructions();
         }
     }
 }
---
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/DelayedDelete.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRChatContentManagerConnect.Editor;
+
+internal static class DelayedDelete {
+    private static readonly Dictionary<string, int> _fileDeleteAttempts = new();
+
+    private const int MaxDeleteAttempts = 5;
+
+    public static void Delete(string path) {
+        RunDeleteAttempts();
+        
+        if (!File.Exists(path)) {
+            throw new FileNotFoundException(path);
+        }
+
+        Debug.Log("[VRCCM.Connect] DelayedDelete: new file to delete: " + path);
+        try {
+            File.Delete(path);
+            Debug.Log("[VRCCM.Connect] DelayedDelete: File deleted: " + path);
+        }
+        catch (Exception ex) {
+            Debug.LogException(ex);
+            Debug.LogWarning("[VRCCM.Connect] DelayedDelete: Failed to delete file, push to delayed delete: " + path);
+            
+            _fileDeleteAttempts[path] = 1;
+        }
+    }
+    
+    [MenuItem("Tools/VRChat Content Manager Connect/Delayed Delete/List Delayed Delete Attempts")]
+    public static void ListDeleteAttempts() {
+        if (_fileDeleteAttempts.Count == 0) {
+            Debug.Log("[VRCCM.Connect] DelayedDelete: No files to delete.");
+            return;
+        }
+        
+        var stringBuilder = new StringBuilder();
+        
+        stringBuilder.AppendLine("[VRCCM.Connect] DelayedDelete: Files pending deletion:");
+        foreach (var kvp in _fileDeleteAttempts) {
+            stringBuilder.AppendLine($"  {kvp.Key} - Attempts: {kvp.Value}");
+        }
+        
+        Debug.Log(stringBuilder.ToString());
+    }
+
+    [MenuItem("Tools/VRChat Content Manager Connect/Delayed Delete/Run Delayed Delete Attempts")]
+    public static void RunDeleteAttempts() {
+        var filesToDelete = _fileDeleteAttempts.Keys.ToList();
+
+        if (filesToDelete.Count == 0) {
+            Debug.Log("[VRCCM.Connect] DelayedDelete: No files to delete.");
+            return;
+        }
+
+        foreach (var filePath in filesToDelete) {
+            try {
+                Debug.Log("[VRCCM.Connect] DelayedDelete: Try Deleting file: " + filePath);
+                if (File.Exists(filePath)) {
+                    File.Delete(filePath);
+                    Debug.Log("[VRCCM.Connect] DelayedDelete: File deleted: " + filePath);
+                    _fileDeleteAttempts.Remove(filePath);
+                }
+                else {
+                    Debug.LogWarning("[VRCCM.Connect] DelayedDelete: File not found: " + filePath);
+                    _fileDeleteAttempts.Remove(filePath);
+                }
+            }
+            catch (Exception ex) {
+                Debug.LogException(ex);
+                Debug.LogWarning("[VRCCM.Connect] DelayedDelete: Failed to delete file: " + filePath);
+                _fileDeleteAttempts[filePath]++;
+                if (_fileDeleteAttempts[filePath] >= MaxDeleteAttempts) {
+                    Debug.LogWarning("[VRCCM.Connect] DelayedDelete: Max delete attempts reached for file: " +
+                                     filePath);
+                    _fileDeleteAttempts.Remove(filePath);
+                }
+            }
+        }
+    }
+}
