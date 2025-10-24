3700e98354d043a5ddb1a9deeff15ba49cefa07c
Misaka-L <lipww1234@foxmail.com>
2025-10-24 15:46:52 +0800

    feat: open application temp folder menu

---
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/OpenApplicationTempFolder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace VRChatContentManagerConnect.Editor;
+
+internal static class OpenApplicationTempFolder {
+    [MenuItem("Tools/VRChat Content Manager Connect/Open Application Temp Folder")]
+    public static void Open() {
+        Application.OpenURL(Application.temporaryCachePath);
+    }
+}
