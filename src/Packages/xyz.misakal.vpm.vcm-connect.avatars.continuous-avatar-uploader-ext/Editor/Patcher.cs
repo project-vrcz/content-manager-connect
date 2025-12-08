37926439bdfd0dda2bfa83e0ec91629bef65949a
Misaka-L <lipww1234@foxmail.com>
2025-12-09 02:35:53 +0800

    feat: show rpc status in cau gui and check rpc status before cau start upload

---
+++ src/Packages/xyz.misakal.vpm.vcm-connect.avatars.continuous-avatar-uploader-ext/Editor/Patcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace VRChatContentManagerConnect.Avatars.Editor.ContinuousAvatarUploader;
+
+internal sealed class Patcher {
+    [InitializeOnLoadMethod]
+    private static void InitializeOnLoadMethod() {
+        var harmony = new HarmonyLib.Harmony("xyz.misakal.vpm.vcm-connect.avatars.continuous-avatar-uploader.patcher");
+            
+        harmony.PatchAll();
+    }
+}
---
+++ src/Packages/xyz.misakal.vpm.vcm-connect.avatars.continuous-avatar-uploader-ext/Editor/RpcClientServiceInstance.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VRChatContentManagerConnect.Editor;
+using VRChatContentManagerConnect.Editor.Services.Rpc;
+
+namespace VRChatContentManagerConnect.Avatars.Editor.ContinuousAvatarUploader;
+
+internal static class RpcClientServiceInstance {
+    private static RpcClientService? _rpcClientService;
+    
+    public static RpcClientService? TryGetRpcClientService() {
+        if (_rpcClientService != null)
+            return _rpcClientService;
+
+        if (ConnectEditorApp.Instance is not { } app) {
+            return null;
+        }
+
+        _rpcClientService = app.ServiceProvider.GetRequiredService<RpcClientService>();
+        return _rpcClientService;
+    }
+}
