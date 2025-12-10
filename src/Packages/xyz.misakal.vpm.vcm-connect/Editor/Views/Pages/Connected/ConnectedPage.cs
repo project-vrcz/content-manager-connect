0f38354edf4b0576e37a1168de74919081c70d8a
Misaka-L <lipww1234@foxmail.com>
2025-12-11 02:10:40 +0800

    feat: new settings ui

---
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/Views/Pages/Connected/ConnectedPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRChatContentManagerConnect.Editor.Services.Rpc;
+
+namespace VRChatContentManagerConnect.Editor.Views.Pages.Connected;
+
+internal sealed class ConnectedPage : VisualElement {
+    private readonly RpcClientService _rpcClientService;
+
+    private readonly Label _instanceNameLabel;
+    private readonly Button _disconnectButton;
+
+    private const string VisualTreeAssetGuid = "53ac2986a9ff4c81bb6ee5ff9978953c";
+
+    public ConnectedPage() {
+        var path = UnityEditor.AssetDatabase.GUIDToAssetPath(VisualTreeAssetGuid);
+        var visualTreeAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+
+        visualTreeAsset.CloneTree(this);
+
+        _instanceNameLabel = this.Q<Label>("instance-name");
+        _disconnectButton = this.Q<Button>("disconnect-button");
+
+        if (ConnectEditorApp.Instance is not { } app) {
+            Debug.LogWarning("ConnectEditorApp instance is not available.");
+            throw new InvalidOperationException("ConnectEditorApp instance is not available.");
+        }
+
+        _rpcClientService = app.ServiceProvider.GetRequiredService<RpcClientService>();
+
+        _disconnectButton.clicked += async () => await _rpcClientService.DisconnectAsync();
+
+        RegisterCallback<AttachToPanelEvent>(_ => {
+            _rpcClientService.StateChanged += OnRpcClientServiceStateChanged;
+        });
+
+        RegisterCallback<DetachFromPanelEvent>(_ => {
+            _rpcClientService.StateChanged -= OnRpcClientServiceStateChanged;
+        });
+
+        UpdateUi();
+    }
+
+    private void UpdateUi() {
+        if (_rpcClientService.State != RpcClientState.Connected || _rpcClientService.InstanceName is null) {
+            _instanceNameLabel.text = "Invalid Status, Try reconnect?";
+        }
+
+        _instanceNameLabel.text = _rpcClientService.InstanceName;
+    }
+
+    private void OnRpcClientServiceStateChanged(object sender, RpcClientState e) {
+        MainThreadDispatcher.Dispatch(UpdateUi);
+    }
+}
---
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/Views/Pages/NewConnection/NewConnectionPage.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRChatContentManagerConnect.Editor.Services.Rpc;
+
+namespace VRChatContentManagerConnect.Editor.Views.Pages.NewConnection;
+
+internal sealed class NewConnectionPage : VisualElement {
+    private const string VisualTreeAssetGuid = "280eea869a434efd8a9abf4dd83eee43";
+
+    private readonly RpcClientService _rpcClientService;
+
+    private readonly TextField _hostField;
+    private readonly Button _connectButton;
+
+    private readonly VisualElement _connectContainer;
+    private readonly VisualElement _challengeContainer;
+
+    private readonly Label _identityPromptLabel;
+    private readonly TextField _challengeCodeInputField;
+    private readonly Button _challengeButton;
+    private readonly Button _cancelChallengeButton;
+
+    public NewConnectionPage() {
+        var assetPath = UnityEditor.AssetDatabase.GUIDToAssetPath(VisualTreeAssetGuid);
+        var visualTreeAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(assetPath);
+        visualTreeAsset.CloneTree(this);
+
+        _hostField = this.Q<TextField>("host-input-field");
+        _connectButton = this.Q<Button>("connect-button");
+
+        _connectContainer = this.Q<VisualElement>("connect-container");
+        _challengeContainer = this.Q<VisualElement>("challenge-container");
+
+        _identityPromptLabel = this.Q<Label>("identity-prompt-label");
+        _challengeCodeInputField = this.Q<TextField>("code-input-field");
+        _challengeButton = this.Q<Button>("challenge-button");
+        _cancelChallengeButton = this.Q<Button>("cancel-challenge-button");
+
+        if (ConnectEditorApp.Instance is not { } app) {
+            Debug.LogWarning("ConnectEditorApp instance is not available.");
+            throw new InvalidOperationException("ConnectEditorApp instance is not available.");
+        }
+
+        _rpcClientService = app.ServiceProvider.GetRequiredService<RpcClientService>();
+
+        RegisterCallback<AttachToPanelEvent>(_ => {
+            _rpcClientService.StateChanged += OnRpcClientServiceStateChanged;
+        });
+
+        RegisterCallback<DetachFromPanelEvent>(_ => {
+            _rpcClientService.StateChanged -= OnRpcClientServiceStateChanged;
+        });
+
+        _connectButton.clicked += async () => {
+            try {
+                await _rpcClientService.RequestChallengeAsync(_hostField.value);
+            }
+            catch (Exception ex) {
+                Debug.LogError($"Failed to request challenge: {ex}");
+                Debug.LogException(ex);
+
+                UnityEditor.EditorUtility.DisplayDialog(
+                    "Connection Error",
+                    $"Failed to connect to {_hostField.value}:\n\n{ex}",
+                    "OK");
+            }
+        };
+
+        _challengeButton.clicked += async () => {
+            try {
+                await _rpcClientService.CompleteChallengeAsync(_challengeCodeInputField.text);
+            }
+            catch (Exception ex) {
+                Debug.LogError($"Failed to complete challenge: {ex}");
+                Debug.LogException(ex);
+
+                UnityEditor.EditorUtility.DisplayDialog(
+                    "Challenge Error",
+                    $"Failed to complete challenge:\n\n{ex}",
+                    "OK");
+            }
+        };
+
+        _cancelChallengeButton.clicked += async () => {
+            try {
+                await _rpcClientService.DisconnectAsync();
+            }
+            catch (Exception ex) {
+                Debug.LogError("Failed to cancel challenge: " + ex);
+                Debug.LogException(ex);
+
+                UnityEditor.EditorUtility.DisplayDialog(
+                    "Cancel Challenge Error",
+                    $"Failed to cancel challenge:\n\n{ex}",
+                    "OK");
+            }
+        };
+
+        UpdateUi();
+    }
+
+    private void UpdateUi() {
+        _identityPromptLabel.text =
+            _rpcClientService.GetIdentityPrompt() ?? "Invalid Status, Try restart connect process?";
+
+        if (_rpcClientService.State == RpcClientState.AwaitingChallenge) {
+            _connectContainer.style.display = DisplayStyle.None;
+            _challengeContainer.style.display = DisplayStyle.Flex;
+        }
+        else {
+            _connectContainer.style.display = DisplayStyle.Flex;
+            _challengeContainer.style.display = DisplayStyle.None;
+        }
+    }
+
+    private void OnRpcClientServiceStateChanged(object sender, RpcClientState e) {
+        MainThreadDispatcher.Dispatch(UpdateUi);
+    }
+}
