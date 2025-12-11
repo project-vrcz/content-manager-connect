bc8ba19e6b2f51d084d1d21e46bbd840f16a8ceb
Misaka-L <lipww1234@foxmail.com>
2025-12-12 01:12:03 +0800

    feat: show rpc connection status and restore connection in toolbar

--- src/Packages/xyz.misakal.vpm.vcm-connect/Editor/Services/EditorAppLifetimeService.cs
+++ src/Packages/xyz.misakal.vpm.vcm-connect/Editor/Services/EditorAppLifetimeService.cs
@@ -8,13 +8,19 @@ namespace VRChatContentManagerConnect.Editor.Services;
 internal sealed class EditorAppLifetimeService {
     private readonly RpcClientService _rpcClientService;
     private readonly AppSettingsService _appSettingsService;
+    private readonly MenuItemService _menuItemService;
 
-    public EditorAppLifetimeService(RpcClientService rpcClientService, AppSettingsService appSettingsService) {
+    public EditorAppLifetimeService(
+        RpcClientService rpcClientService,
+        AppSettingsService appSettingsService,
+        MenuItemService menuItemService) {
         _rpcClientService = rpcClientService;
         _appSettingsService = appSettingsService;
+        _menuItemService = menuItemService;
     }
 
     public async Task StartAsync() {
+        _menuItemService.Init();
         _appSettingsService.GetSettings();
 
         try {
